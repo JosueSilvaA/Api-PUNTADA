18d94ded013cc49c6b8393635cfe674b39c2aee8
JosueSilvaA <josuemarkcs@gmail.com>
2020-07-02 16:03:51 -0600

    Creado modelo usuario

---
+++ src/models/usuario.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const usuario = new mongoose.Schema({
+    nombres:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    apellido:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    usuario:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    direccion:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    correo:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    contrasena:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    rol:{
+        type:Number,
+        required:true,
+        trim:true
+    },
+    identidad:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    telefono:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    estado:{
+        type:Boolean,
+        trim:true,
+        default:true
+    },
+    creada: { 
+        type: Date,
+        default: Date.now
+    },
+    conexiones:mongoose.SchemaTypes.Array
+});
+
+module.exports = mongoose.model('usuario',usuario);
