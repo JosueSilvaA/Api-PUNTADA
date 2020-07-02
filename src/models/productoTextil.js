db6bb02963f6893368fc9e2c8b14c4d76af0584b
JosueSilvaA <josuemarkcs@gmail.com>
2020-07-02 16:57:08 -0600

    Creado modelo Producto Textil

---
+++ src/models/productoTextil.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const productoTextil = new mongoose.Schema({
+    nombre:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    tipoTextil:{
+        type:mongoose.SchemaTypes.Array,
+        required:true
+    },
+    color:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    rbaColor:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    proveedor:{
+        type:mongoose.SchemaTypes.ObjectId,
+        required:true
+    },
+    precio:{
+        type:mongoose.SchemaTypes.Decimal128,
+        required:true,
+        trim:true
+    },
+    descripcion:{
+        type:String,
+        required:true
+    },
+    estado:{
+        type:mongoose.SchemaTypes.Boolean,
+        default:true
+    },
+    creada:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model('productoTextil',productoTextil);
