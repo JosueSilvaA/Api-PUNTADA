bdb1f41a08694a1a7c008326bf66a6aaa5fbd33a
JosueSilvaA <josuemarkcs@gmail.com>
2020-07-02 13:01:08 -0600

    Creada la conexion a la base de datos e instalado el paquete mongoose

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+var database = require('./connection/connection');
+database();
 /* Import Rutas */
 
 
---
+++ src/connection/connection.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+
+
+const URI = "mongodb+srv://silva:wI58OeWxGVx98coq@puntadadb.xffos.mongodb.net/puntadadb?retryWrites=true&w=majority"
+const connectDB = async () =>{
+    await mongoose.connect(URI,{
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }).then(result=>{
+        console.log('Se conecto a mongoAtlas');
+    })
+    .catch(error =>{
+        console.log(error);
+    });
+};
+
+module.exports = connectDB;
