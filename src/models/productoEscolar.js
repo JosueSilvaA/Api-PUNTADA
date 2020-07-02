1cef1fa94c6e654e4a786f06d1d6710cc82da0d2
JosueSilvaA <josuemarkcs@gmail.com>
2020-07-02 16:06:09 -0600

    Creado modelo Producto Escolar

---
+++ src/models/productoEscolar.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const productoEscolar = new mongoose.Schema({
+    nombre:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    marca:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    proveedor:{
+        type:mongoose.SchemaTypes.ObjectId,
+        required:true
+    },
+    precio:{
+        type:mongoose.SchemaTypes.Decimal128,
+        required:true,
+        trim:true
+    },
+    tipoUtil:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    descripcion:{
+        type:String,
+        required:true
+    },
+    estado:{
+        type:mongoose.SchemaTypes.Boolean,
+        required:true,
+        default:true
+    },
+    creada:{ 
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('productoEscolar',productoEscolar);
