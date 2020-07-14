ddcbc6471d0497f164d3be07b3d5daeeaf34be71
josue silva <josuemarkcs@gmail.com>
2020-07-13 18:53:48 -0600

    Creado servicio de registro de productos escolares

--- src/routes/productosEscolares.js
+++ src/routes/productosEscolares.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 const productoEscolar = require('../models/productoEscolar');
 const Result = require('../helpers/result');
+const mongoose = require('mongoose');
 
 // registrar un producto escolares
 
 router.post('/registroProductoEscolar',function(req,res){
     let result = Result.createResult();
-    
+    let nuevoProducto = new productoEscolar({
+        nombre:req.body.nombre,
+        marca:req.body.marca,
+        proveedor:mongoose.Types.ObjectId(req.body.proveedor),
+        precio:req.body.precio,
+        tipoUtil:req.body.tipoUtil,
+        descripcion:req.body.descripcion
+    });
+    nuevoProducto.save().then(response=>{
+        result.Error = false
+        result.Response = 'Producto Escolar registrado con exito'
+        result.Items = response
+        res.send(result)
+    }).catch(err=>{
+        result.Error = err
+        result.Response = 'Ocurrio un error'
+        result.Success = false
+        res.send(result)
+    })
 });
