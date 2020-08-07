91ce22bdec0a4909018028de5ec048f290ba4f8d
josue silva <josuemarkcs@gmail.com>
2020-08-06 21:22:45 -0600

    Modificacion de campos  del modelo de proveedor

--- src/models/proveedor.js
+++ src/models/proveedor.js
@@ -25,10 +25,14 @@ const proveedor = new mongoose.Schema({
         required:true
     },
     tipoProducto:{
-        type:mongoose.SchemaTypes.Array,
+        type:String,
         trim:true,
-        required:true,
-        default:[]
+        required:true
+    },
+    imgProveedor:{
+        type:String,
+        trim:true,
+        default:'https://www.comologia.com/wp-content/uploads/2018/03/mejores-proveedores-mayoristas-distribuidores-confiables.jpg'
     },
     estado:{
         type:mongoose.SchemaTypes.Boolean,
